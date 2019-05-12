f039fcb4fddb54b83a6629474918f290ab12e298
hp <hp@Peraza>
2019-05-11 18:59:00 -0600

    Proyecto de Examen 2

--- src/exaprac2/ExaPrac2.java
+++ src/exaprac2/ExaPrac2.java
@@ -1,13 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package exaprac2;
 
+import javax.swing.JOptionPane;
+
 /**
- *
- * @author hp
+ * Examen REalizado por David Ernesto Peraza Aguillon cod:373218 y
+                        Alejandro Ernesto Aguirre Angel cod: 370118
  */
 public class ExaPrac2 {
 
@@ -15,7 +13,46 @@ public class ExaPrac2 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        /* Problema: crear un programa que permita registrar datos de una planilla de trabajadores
+("n"trabajadores), por cada empleado se ingresara:Nombre de empleado, salario x hora y
+la cantidad de horas trabajadas.
+el programa debe mostrar al final por medio de un JOptionPane el detalle de la planilla 
+y el total de la planilla. en otro cuadro se mostrara el nombre del empleado que mas gano en esa planilla. **/  
+        
+int cantemple=0;
+double totalplanilla=0;
+double numeromayor=0;
+String masgano = "";
+String detalle="Detalle de Empleados Planillas \n";
+
+cantemple = Integer.parseInt(JOptionPane.showInputDialog("Cuantos Empleados desea Agregar a la Planilla?"));
+
+ 
+ String nombre[]= new String[cantemple];
+ int horastrabajas[]= new int[cantemple];
+ double salario[]=new double [cantemple];
+ double totalsalario[]=new double [cantemple];
+ 
+ for(int i=0; i<cantemple; i++){
+     nombre[i]= JOptionPane.showInputDialog("Nombre de Empleado " + (i+1));
+     salario[i]= Double.parseDouble(JOptionPane.showInputDialog("Salario X hora"));
+     horastrabajas[i]= Integer.parseInt(JOptionPane.showInputDialog("Horas Trabajadas"));
+     totalsalario[i]= salario[i]* horastrabajas[i];
+     totalplanilla += totalsalario[i];
+   detalle += "Empleado"+ (i+ 1) + ": " + nombre[i] + "Salario: $ " + totalsalario[i] +  "\n";
+   
+ }
+     for(int m=0; m<totalsalario.length && m<nombre.length; m++){
+        if(totalsalario[m]>numeromayor){ 
+        numeromayor = totalsalario[m];
+        masgano = nombre[m];
+    }
+}
+         
+   
+ JOptionPane.showMessageDialog(null, detalle + "\n Total de la Planilla es: $" + totalplanilla +"\n EL Empleado que mas gano es: " + masgano);
+ 
+ 
     }
     
 }
